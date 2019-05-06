e01c979b516fdc71e43ebb68f3324200015c4d2e
FarmDoc <33397066+yakushevt@users.noreply.github.com>
2019-05-07 00:55:52 +0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,73 @@
+ function it(l) {
+                return t["ɵvid"](0, [(l()(), t["ɵeld"](0, 0, null, null, 31, "div", [
+                    ["class", "isp-layout__block"]
+                ], null, null, null, null, null)), (l()(), t["ɵted"](-1, null, ["\n          "])), (l()(), t["ɵeld"](2, 0, [
+                    [1, 0],
+                    ["menuBlockElement", 1]
+                ], null, 28, "ul", [
+                    ["class", "isp-menu isp-menu_theme_inline isp-header__menu"]
+                ], null, null, null, null, null)), (l()(), t["ɵted"](-1, null, ["\n            "])), (l()(), t["ɵeld"](4, 0, null, null, 8, "li", [
+                    ["class", "isp-menu__item"]
+                ], null, null, null, null, null)), (l()(), t["ɵted"](-1, null, ["\n              "])), (l()(), t["ɵeld"](6, 0, null, null, 5, "a", [
+                    ["class", "isp-menu__link isp-menu__link_front"],
+                    ["routerLink", "/dashboard"],
+                    ["routerLinkActive", "isp-menu__link_active"]
+                ], [
+                    [1, "target", 0],
+                    [8, "href", 4]
+                ], [
+                    [null, "click"]
+                ], function(l, n, e) {
+                    var u = !0,
+                        i = l.component;
+                    return "click" === n && (u = !1 !== t["ɵnov"](l, 7).onClick(e.button, e.ctrlKey, e.metaKey, e.shiftKey) && u), "click" === n && (u = !1 !== i.onMenuClick(e) && u), u
+                }, null, null)), t["ɵdid"](7, 671744, [
+                    [3, 4]
+                ], 0, P.r, [P.o, P.a, a.i], {
+                    routerLink: [0, "routerLink"]
+                }, null), t["ɵdid"](8, 1720320, null, 2, P.q, [P.o, t.ElementRef, t.Renderer2, t.ChangeDetectorRef], {
+                    routerLinkActive: [0, "routerLinkActive"]
+                }, null), t["ɵqud"](603979776, 2, {
+                    links: 1
+                }), t["ɵqud"](603979776, 3, {
+                    linksWithHrefs: 1
+                }), t["ɵdid"](11, 737280, null, 0, d.a, [t.ElementRef, t.Renderer2, s.k], {
+                    msgName: [0, "msgName"]
+                }, null), (l()(), t["ɵted"](-1, null, ["\n            "])), (l()(), t["ɵted"](-1, null, ["\n            "])), (l()(), t["ɵand"](16777216, null, null, 1, null, Qe)), t["ɵdid"](15, 802816, null, 0, a.k, [t.ViewContainerRef, t.TemplateRef, t.IterableDiffers], {
+                    ngForOf: [0, "ngForOf"]
+                }, null), (l()(), t["ɵted"](-1, null, ["\n            "])), (l()(), t["ɵand"](16777216, null, null, 1, null, ut)), t["ɵdid"](18, 16384, null, 0, a.l, [t.ViewContainerRef, t.TemplateRef], {
+                    ngIf: [0, "ngIf"]
+                }, null), (l()(), t["ɵted"](-1, null, ["\n            "])), (l()(), t["ɵted"](-1, null, ["\n            "])), (l()(), t["ɵeld"](21, 0, null, null, 8, "li", [
+                    ["class", "isp-menu__item"]
+                ], null, null, null, null, null)), (l()(), t["ɵted"](-1, null, ["\n              "])), (l()(), t["ɵeld"](23, 0, null, null, 5, "a", [
+                    ["class", "isp-menu__link isp-menu__link_front"],
+                    ["routerLink", "/finance"],
+                    ["routerLinkActive", "isp-menu__link_active"]
+                ], [
+                    [1, "target", 0],
+                    [8, "href", 4]
+                ], [
+                    [null, "click"]
+                ], function(l, n, e) {
+                    var u = !0,
+                        i = l.component;
+                    return "click" === n && (u = !1 !== t["ɵnov"](l, 24).onClick(e.button, e.ctrlKey, e.metaKey, e.shiftKey) && u), "click" === n && (u = !1 !== i.onMenuClick(e) && u), u
+                }, null, null)), t["ɵdid"](24, 671744, [
+                    [11, 4]
+                ], 0, P.r, [P.o, P.a, a.i], {
+                    routerLink: [0, "routerLink"]
+                }, null), t["ɵdid"](25, 1720320, null, 2, P.q, [P.o, t.ElementRef, t.Renderer2, t.ChangeDetectorRef], {
+                    routerLinkActive: [0, "routerLinkActive"]
+                }, null), t["ɵqud"](603979776, 10, {
+                    links: 1
+                }), t["ɵqud"](603979776, 11, {
+                    linksWithHrefs: 1
+                }), t["ɵdid"](28, 737280, null, 0, d.a, [t.ElementRef, t.Renderer2, s.k], {
+                    msgName: [0, "msgName"]
+                }, null), (l()(), t["ɵted"](-1, null, ["\n            "])), (l()(), t["ɵted"](-1, null, ["\n          "])), (l()(), t["ɵted"](-1, null, ["\n        "]))], function(l, n) {
+                    var e = n.component;
+                    l(n, 7, 0, "/dashboard"), l(n, 8, 0, "isp-menu__link_active"), l(n, 11, 0, "COMMON:MAIN_MENU_HOME"), l(n, 15, 0, e.mainMenuShowing), l(n, 18, 0, e.mainMenuList.length - e.mainMenuShowing.size > 0), l(n, 24, 0, "/finance"), l(n, 25, 0, "isp-menu__link_active"), l(n, 28, 0, "COMMON:MAIN_MENU_FINANCE")
+                }, function(l, n) {
+                    l(n, 6, 0, t["ɵnov"](n, 7).target, t["ɵnov"](n, 7).href), l(n, 23, 0, t["ɵnov"](n, 24).target, t["ɵnov"](n, 24).href)
+                })
+            }
